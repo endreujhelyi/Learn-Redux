c982a0ac1d6dc0e247ce0df2745435aa761b037d
Endre <endreujhelyi@gmail.com>
2017-02-05 00:57:31 +0100

    accessing dispatch and state with Redux

---
+++ client/components/App.js
@@ -0,0 +1,19 @@
+import { bindActionCreators } from 'redux';
+import { connect } from 'react-redux';
+
+import * as actionCreators from '../actions/actionCreators';
+import Main from './Main';
+
+
+const mapStateToProps = (state) => {
+  posts: state.posts;
+  comments: state.comments;
+}
+
+const mapDispatchToProps = (dispatch) =>
+  bindActionCreators(actionCreators, dispatch);
+
+const App = connect(mapDispatchToProps, mapDispatchToProps)(Main);
+
+
+export default App;
