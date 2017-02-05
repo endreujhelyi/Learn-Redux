81408c8be3008f78d7e81cf2c2f78b45668fc6f6
Endre <endreujhelyi@gmail.com>
2017-02-05 14:46:04 +0100

    displaying a single photo comment

--- client/store.js
+++ client/store.js
@@ -15,7 +15,7 @@ const defaultState = {
   comments
 };
 
-const store = createStore(rootReducer, defaultState,     window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
+const store = createStore(rootReducer, defaultState, window.__REDUX_DEVTOOLS_EXTENSION__ && window.__REDUX_DEVTOOLS_EXTENSION__()
 );
 
 
